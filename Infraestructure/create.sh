d68cdf125c05f3e0175d92868f5aa14699d0d802
Amanda Souza <amandasouza@copin.ufcg.edu.br>
2019-07-07 06:34:11 +0300

    fix: typos adding new server

--- Infraestructure/create.sh
+++ Infraestructure/create.sh
@@ -2,4 +2,5 @@ aws cloudformation create-stack \
 --stack-name $1 \
 --template-body file://$2 \
 --parameters file://$3 \
---region=us-west-2
+--region us-west-2 \
+--capabilities CAPABILITY_NAMED_IAM
--- Infraestructure/update.sh
+++ Infraestructure/update.sh
@@ -2,4 +2,5 @@ aws cloudformation update-stack \
 --stack-name $1 \
 --template-body file://$2 \
 --parameters file://$3 \
---region=us-west-2
+--region us-west-2 \
+--capabilities CAPABILITY_NAMED_IAM
